d0316c17176a59bb8756074d2f51aa7e81e2f670
Fraser Cripps <56896998+fraserac@users.noreply.github.com>
2021-04-09 14:56:30 +0200

    Add files via upload

--- ENERGIE_REST_API.py
+++ ENERGIE_REST_API.py
@@ -12,10 +12,8 @@ Merit order important
 import flask
 from flask import Flask, request, jsonify
 app = Flask(__name__)
-import pandas as pd
-import operator
 from collections import OrderedDict
-#, name, pType, efficiency, pmin, pmax, fuels
+
 class PowerPlant(object):
      
     def __init__(self):
@@ -127,6 +125,7 @@ class PowerPlant(object):
             self._meritVal = (1/self._efficiency*self._fuelCost) + self._c02Cost
         elif self._pType == 'windturbine':
             self._meritVal = 0 
+            
     
     def Carbon_Cost(self):
         if self._pType == 'gasfired':
@@ -140,7 +139,7 @@ class PowerPlant(object):
     
 @app.route('/productionplan/', methods= ['GET', 'POST'])
 def Production_Plan():
-    
+        
         fuelsDict ={"fuels" : {"gas(euro/MWh)" : 0, "kerosine(euro/MWh)" : 0,
                                          "co2(euro/ton)":0,"wind(%)":0}  } 
         
@@ -161,103 +160,54 @@ def Production_Plan():
         
         databaseDict = {x['name'] :[] for x in powerplantsDict['powerplants']}
         
-            
-        #permList = OnoffPermuter(databaseDict, len(powerplantsDict['powerplants']))
-        
-        
-        # list comprehension combined with dict comprehension?
-        
         key_list = ['name', 'p']
         
         
         nameList = [x['name'] for x in powerplantsDict['powerplants']]
         instanceDict = {}
-        
+
         for i in range(len(nameList)):
             instanceDict['PowerPlant_%s' % str(i)] = PowerPlant()
             instanceDict['PowerPlant_%s' % str(i)]._name = powerplantsDict['powerplants'][i]['name']
             instanceDict['PowerPlant_%s' % str(i)]._pType =  powerplantsDict['powerplants'][i]['type']
             instanceDict['PowerPlant_%s' % str(i)]._efficiency = powerplantsDict['powerplants'][i]['efficiency'] 
             instanceDict['PowerPlant_%s' % str(i)]._pmin = powerplantsDict['powerplants'][i]['pmin']
-            instanceDict['PowerPlant_%s' % str(i)]._pmax = powerplantsDict['powerplants'][i]['pmax'] 
+            if instanceDict['PowerPlant_%s' % str(i)]._pType =='windturbine':
+                instanceDict['PowerPlant_%s' % str(i)]._pmax = int(powerplantsDict['powerplants'][i]['pmax']*(fuelsDict['fuels']["wind(%)"]/100))
+            else:
+                instanceDict['PowerPlant_%s' % str(i)]._pmax = powerplantsDict['powerplants'][i]['pmax'] 
+           
             instanceDict['PowerPlant_%s' % str(i)]._fuels = fuelsDict['fuels']
             instanceDict['PowerPlant_%s' % str(i)].Carbon_Cost()
             instanceDict['PowerPlant_%s' % str(i)].Choose_Fuel()
             instanceDict['PowerPlant_%s' % str(i)].Merit_Value()
             
-            
-        # dictionary of powerplant objects created. 
-        
-        #costEff=Build_Cost_Eff(nameList, powerplantsDict, fuelsDict['fuels'])
-        #euro per ton of co2 and wind% passed through
-        #merCond ={'co2(euro/ton)': fuelsDict['fuels']['co2(euro/ton)'], 'wind(%)': fuelsDict['fuels']['wind(%)']}
-        
-        meritOrd = Merit_Order(instanceDict)#(merCond, costEff, nameList, powerplantsDict)
-        
-        #Tomorrow use meritOrd to assign power load, use load - sum(pmin) so need powerplantsDict
-        
-        
-        pDict = Unit_Commit(meritOrd, dataOuterDict['load']) #must contain all names!
-        
-        #listOut = [{key_list[0]: nameList[idx], key_list[1]: pList[idx]} for idx in range(len(nameList))]
+       
+        meritOrd = Merit_Order(instanceDict)
+        pDict, errorCode = Unit_Commit(meritOrd, dataOuterDict['load']) 
+       
        
         if flask.request.method == 'POST':
-            return jsonify(pDict), 200
-        
-        
-        
-        
-# def Build_Cost_Eff(names, powDict, costs):
-#     #Here extract relevant info
-#     #first extract from fuels gas, kerosine info
-#     #attach to key: name of powerplant
-#     #for wp1, wp2 do same operation except divide % by 100
-   
-#     typeDict = {'gasfired': costs['gas(euro/MWh)'], 'turbojet': costs['kerosine(euro/MWh)'], 'windturbine': costs['wind(%)']/100}
-#     costEff = {x : [] for x in names}
-#     for items in powDict['powerplants']: 
-#         costEff[items['name']].append(items['type'])
-#         costEff[items['name']].append(items['efficiency'])
-#         for key in typeDict:
-#             if costEff[items['name']][0] == key:
-#                 costEff[items['name']].append(typeDict[key])
-#     return costEff
-   
+            return jsonify(pDict), errorCode
 
-def Merit_Order(objDict):#merCond, costEff, names, powDict):  # take in dict containing cost per MWh and eff for given name 
+def Merit_Order(objDict):
     meritOrderUnSorted={}
-   # breakpoint()
     listOfObj = list(objDict.values())
-    
-   # for key in objDict:
-       # meritOrderUnSorted[objDict[key]= objDict[key]._meritVal
-   # keyToDel = []
-    # for key in costEff:
-    #     name = key
-    #     costPMWh = costEff[key][2]
-    #     meritCoEf = 1/costEff[key][1]
-    #     meritOrderUnSorted[name] = costPMWh*meritCoEf
-    # for key in meritOrderUnSorted:
-    #     if meritOrderUnSorted[key] ==0:
-    #         keyToDel.append(key)
-    # for i in keyToDel:
-    #     del meritOrderUnSorted[i]
-    #merOrd =OrderedDict()
     listOfObj.sort(key=lambda x: x._meritVal, reverse=False)
-    #merOrd = {x._meritVal : x for x in listOfObj}#{sorted(meritOrderUnSorted.items(), key=operator.itemgetter(1))}
     return listOfObj
 
 
-def Unit_Commit(mer_Ord, load):
+def Unit_Commit(mer_Ord, load): 
+   errorCode =200
+   maxPow = 0
+   for k in range(len(mer_Ord)):
+       maxPow += mer_Ord[k]._pmax
+   if maxPow < load:
+       app.logger.error('Load too high for available power plants to meet demand.')
+       
    initialLoad = load
-    # Ignores spinning reserve principle, load - sum pmin of all in merit OrderedDict, from first to last of merit 
-    #order if PMax of an item < than load -sum pmin, then next item. If PMax of new item is more than needed, just use amount 
-    #needed
-    #Unit Commit output can pretty much go straight into listOut without total cost 
    pDict = [OrderedDict({'name' : "", 'p':0}) for i in range(len(mer_Ord))]
-   #breakpoint()
-   
-   noNeg = 'null'
+   noNeg = 'null' # used to find index where pmin exceeds load remaining to supply
    for i in range(len(mer_Ord)):
        if mer_Ord[i].pmax <= load:
            pDict[i]['name'] = mer_Ord[i]._name
@@ -282,13 +232,8 @@ def Unit_Commit(mer_Ord, load):
           
    if not noNeg == 'null' and pDict[noNeg]['p'] < mer_Ord[noNeg]._pmin:
          remainder = (mer_Ord[noNeg]._pmin-abs(load))
-         print("remainder", remainder)
          pDict[noNeg-1]['p'] -= remainder
          pDict[noNeg]['p'] += remainder
-        #app.logger.error('Load too high for all power plants to meet demand.')
-   elif load <0 and len(pDict)>1:
-        # decrease power from second worst merit power source to meet supply.
-        pass
    elif load <0 and len(pDict) <=1:
         app.logger.error('Load is below Pmin of all plants.')
    check =0
@@ -296,8 +241,8 @@ def Unit_Commit(mer_Ord, load):
         check += pDict[i]['p'] 
    if check > initialLoad or check < initialLoad:
         app.logger.error('Final output does not match load. Internal error. ')
-   print(check)
-   return pDict
+        errorCode =500
+   return pDict, errorCode
 
 
 
